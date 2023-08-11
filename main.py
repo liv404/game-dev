a0ac1c7f7e6598868a94c0c9677be4a0ae58572a
liv404 <120323880+liv404@users.noreply.github.com>
2023-08-11 09:52:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ class Magic_Ball(pygame.sprite.Sprite):
 
     def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.image.load("img/8.png").convert_alpha()
+        self.image = pygame.image.load("8.png").convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
